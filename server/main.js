dd719be93c44c810d7e3648724fe45660f2ccc55
Code Whisperer <daniel@danielstern.ca>
2015-07-16 19:26:15 -0400

    payload readied

--- server/main.js
+++ server/main.js
@@ -3,16 +3,26 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-// app.get('/',function(req,res){
-//   res.sendFile(__dirname +'/app/index.html',{root:'../'});
-// });
-
 app.use(express.static('app'));
 app.use('/bower_components',express.static('bower_components'));
 server.listen(80);
 
+var seed = [{
+  sender:"Jack Burton",
+  content:{text:"Everybody relax, I'm here."},
+  ts:0
+},{
+  sender:"Jake Gittes",
+  content:{text:"You're dumber than you think I think you are."},
+  ts:100
+},{
+  sender:"Zachariah Rigby",
+  content:{text:"You think I'm not serious just because I carry a rabbit?"},
+  ts:100
+}];
+
 io.on('connection',function(socket){
-  socket.emit('messages',[{sender:"Jack Burton",content:{text:"Everybody relax, I'm here."},ts:0}]);
+  socket.emit('messages',seed);
   socket.on('new-message', function (data) {
     console.log('new message',data);
   });
