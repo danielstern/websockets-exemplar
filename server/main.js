a03ebe2c9233fcaddfd29153b687d487f49fa810
Code Whisperer <daniel@danielstern.ca>
2015-07-16 19:43:35 -0400

    user intercommunication between browsers

--- server/main.js
+++ server/main.js
@@ -7,7 +7,7 @@ app.use(express.static('app'));
 app.use('/bower_components',express.static('bower_components'));
 server.listen(80);
 
-var seed = [{
+var messages = [{
   sender:"Jack Burton",
   content:{text:"Everybody relax, I'm here."},
   ts:0
@@ -22,8 +22,13 @@ var seed = [{
 }];
 
 io.on('connection',function(socket){
-  socket.emit('messages',seed);
+  console.info("User connected");
+  socket.emit('messages',messages);
   socket.on('new-message', function (data) {
-    console.log('new message',data);
+    console.log('New Message',data.content.text);
+    messages.push(data);
+    io.sockets.emit('messages',messages);
   });
 })
+
+console.log("Server listening on port 80");
