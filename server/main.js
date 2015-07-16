7b7615e3b3c2d3aaa349e40a88e51c42a398f045
Code Whisperer <daniel@danielstern.ca>
2015-07-16 16:57:35 -0400

    backend listener set up

--- server/main.js
+++ server/main.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var io = require('socket.io');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var app = new express();
 app.get('/',function(req,res){
   res.send("Hello, world!");
 });
 app.listen(80);
+
+io.on('connection',function(socket){
+  socket.emit('messages',[{sender:"Jack Burton",content:{text:"Everybody relax, I'm here."},ts:0}]);
+  socket.on('new-message', function (data) {
+    console.log('new message',data);
+  });
+})
