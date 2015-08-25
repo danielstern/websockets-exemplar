2575c9cc763f896111f5b56f75ab060d86fdddb0
Code Whisperer <daniel@danielstern.ca>
2015-08-25 08:00:18 -0400

    socket io on server side

--- server/main.js
+++ server/main.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/',function(req,res){
     res.send("Hello world.");
     console.log("Something connected to express.");
 });
+
+io.on('connection',function(socket){
+    console.log("Something connected to Socket.io");
+    socket.emit("messages",["Hello","Hi there","How are you?"]);
+})
 
 server.listen(80);
