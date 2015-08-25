f74cd3d7aab326841556843a4c089ff66a46a786
Code Whisperer <daniel@danielstern.ca>
2015-08-25 08:12:12 -0400

    socket front end scaffold done

--- server/main.js
+++ server/main.js
@@ -3,10 +3,12 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.get('/',function(req,res){
-    res.send("Hello world.");
-    console.log("Something connected to express.");
-});
+//app.get('/',function(req,res){
+//    res.send("Hello world.");
+//    console.log("Something connected to express.");
+//});
+
+app.use(express.static('app'));
 
 io.on('connection',function(socket){
     console.log("Something connected to Socket.io");
