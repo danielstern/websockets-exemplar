f6412dd5972c7d08150eafb31c75a048f5400993
Code Whisperer <daniel@danielstern.ca>
2015-08-26 08:36:16 -0400

    send-messages

--- app/main.js
+++ app/main.js
@@ -1,4 +1,31 @@
 var socket = io.connect('http://localhost',{'forceNew':true});
 socket.on("messages", function(data){
     console.info(data);
-})
+    
+    var html = data.map(function(data){
+        return (`
+            <div class='name'>
+                ${data.userName}
+            </div>
+           <a href=${data.content.link} class='message' target=blank>
+                ${data.content.text}
+            </a>
+        `)
+    }).join(" ");
+    
+    document.getElementById("messages").innerHTML = html;
+    
+    
+});
+
+function addMessage(e){
+    var payload:{
+        userName:document.getElementById("username").value,
+        content: {
+            text:document.getElementById("message").value,
+            link:document.getElementById("linkAddress").value
+        },
+        ts: Date.now()
+    }
+    return false;
+}
--- server/main.js
+++ server/main.js
@@ -6,9 +6,42 @@ var io = require('socket.io')(server);
 
 app.use(express.static('app'));
 
+var messages = [{
+  userId:1,
+  messageId:10,
+  userName:"Asha Greyjoy",
+  content:{
+    text:"The stone tree of the Stonetrees.",
+    link:"http://awoiaf.westeros.org/index.php/House_Stonetree"
+  },
+  likedBy:[1],
+  ts:Date.now() - 10000
+},{
+  userId:2,
+  messageId:11,
+  userName:"Arya Stark",
+  content:{
+    text:"We'll come see this inn.",
+    link:"http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads"
+  },
+  likedBy:[2,3],
+  ts:Date.now() - 100000
+},{
+  userId:3,
+  messageId:14,
+  userName:"Cersei Lannister",
+  content:{
+    text:"Her scheming forced this on me.",
+    link:"http://gameofthrones.wikia.com/wiki/Margaery_Tyrell"
+  },
+  likedBy:[],
+  ts:Date.now() - 1000000
+}];
+
+
 io.on('connection',function(socket){
     console.log("Something connected to Socket.io");
-    socket.emit("messages",["Hello","Hi there","How are you?"]);
+    socket.emit("messages",messages);
 })
 
 server.listen(80);
